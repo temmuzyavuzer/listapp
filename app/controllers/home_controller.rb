4b5cadfefc020534cc986010aae38fb1a141fac0
Temmuz Yavuzer <temmuzyavuzer@gmail.com>
2021-03-05 11:19:15 +0300

    associations

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,4 +4,7 @@ class HomeController < ApplicationController
   
   def about
   end
+
+  
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_04_094902) do
+ActiveRecord::Schema.define(version: 2021_03_05_073250) do
 
   create_table "lists", force: :cascade do |t|
     t.string "first_name"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 2021_03_04_094902) do
     t.string "twitter"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_lists_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
