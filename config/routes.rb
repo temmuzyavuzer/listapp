6acb2cb1dce4548b32f1a6b3b88021e721355744
Temmuz Yavuzer <temmuzyavuzer@gmail.com>
2021-03-05 12:01:07 +0300

    style edit

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   resources :lists
   #get 'home/index'
   get 'home/about'
-  root 'home#index'
+  #root 'home#index'
+  root 'lists#index'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
